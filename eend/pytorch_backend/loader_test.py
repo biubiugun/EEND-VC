c8ac7a7c8e357a674e1b9f8ee5567fa374e295e8
bhl66 <bhl66@d3-hpc-sjtu-pv-001.cm.cluster>
2023-05-15 19:29:25 +0800

    test data shape

--- eend/pytorch_backend/loader_test.py
+++ eend/pytorch_backend/loader_test.py
@@ -11,6 +11,8 @@ from eend.pytorch_backend.diarization_dataset \
     import DiarizationDatasetFromWave, DiarizationDatasetFromFeat
 import padertorch as pt
 import padertorch.train.optimizer as pt_opt
+from eend import feature
+from eend import kaldi_data
 import yamlargparse
 
 parser = yamlargparse.ArgumentParser(description='training')
@@ -72,6 +74,167 @@ parser.add_argument('--test-run', default=0, type=int, choices=[0, 1],
 
 args = parser.parse_args()
 print(args)
+
+def _count_frames(data_len, size, step):
+    return int((data_len - size + step) / step)
+
+
+def _gen_frame_indices(data_length, size=2000, step=2000):
+    i = -1
+    for i in range(_count_frames(data_length, size, step)):
+        yield i * step, i * step + size
+
+    if i * step + size < data_length:
+        if data_length - (i + 1) * step > 0:
+            if i == -1:
+                yield (i + 1) * step, data_length
+            else:
+                yield data_length - size, data_length
+
+class DiarizationDatasetFromWaveTest(torch.utils.data.Dataset):
+    def __init__(
+            self,
+            data_dir,
+            dtype=np.float32,
+            chunk_size=2000,
+            context_size=0,
+            frame_size=1024,
+            frame_shift=256,
+            subsampling=1,
+            rate=16000,
+            input_transform=None,
+            n_speakers=None,
+            ):
+        self.data_dir = data_dir
+        self.dtype = dtype
+        self.chunk_size = chunk_size
+        self.context_size = context_size
+        self.frame_size = frame_size
+        self.frame_shift = frame_shift
+        self.subsampling = subsampling
+        self.rate = rate
+        self.input_transform = input_transform
+        self.n_speakers = n_speakers
+
+        self.chunk_indices = []
+        self.data = kaldi_data.KaldiData(self.data_dir)
+        self.all_speakers = sorted(self.data.spk2utt.keys())
+        self.all_n_speakers = len(self.all_speakers)
+        self.all_n_speakers_arr =\
+            np.arange(self.all_n_speakers,
+                      dtype=np.int64).reshape(self.all_n_speakers, 1)
+
+        # Make chunk indices: filepath, start_frame, end_frame
+        # for rec in self.data.wavs:
+        #     data_len = int(self.data.reco2dur[rec] * self.rate / frame_shift)
+        #     data_len = int(data_len / self.subsampling)
+        #     for st, ed in _gen_frame_indices(data_len, chunk_size, chunk_size):
+        #         self.chunk_indices.append(
+        #             (rec, st * self.subsampling, ed * self.subsampling))
+        for i,rec in enumerate(self.data.wavs):
+            self.chunk_indices.append([])
+            data_len = int(self.data.reco2dur[rec] * self.rate / frame_shift)
+            data_len = int(data_len / self.subsampling)
+            for st, ed in _gen_frame_indices(data_len, chunk_size, chunk_size):
+                self.chunk_indices[i].append(
+                    (rec, st * self.subsampling, ed * self.subsampling)
+                )
+        print(len(self.chunk_indices), " chunks")
+
+    def __len__(self):
+        return len(self.chunk_indices)
+
+    def __getitem__(self, i):
+        # rec, st, ed = self.chunk_indices[i]
+        # filtered_segments = self.data.segments[rec]
+        # # speakers: the value given from data
+        # speakers = np.unique(
+        #     [self.data.utt2spk[seg['utt']] for seg in filtered_segments]
+        #     ).tolist()
+        # n_speakers = self.n_speakers
+        # if self.n_speakers < len(speakers):
+        #     n_speakers = len(speakers)
+
+        # Y, T = feature.get_labeledSTFT(
+        #     self.data,
+        #     rec,
+        #     st,
+        #     ed,
+        #     self.frame_size,
+        #     self.frame_shift,
+        #     n_speakers,
+        #     )
+        # T = T.astype(np.float32)
+
+        # S_arr = -1 * np.ones(n_speakers).astype(np.int64)
+        # for seg in filtered_segments:
+        #     speaker_index = speakers.index(self.data.utt2spk[seg['utt']])
+        #     all_speaker_index = self.all_speakers.index(
+        #         self.data.utt2spk[seg['utt']])
+        #     S_arr[speaker_index] = all_speaker_index
+
+        # # If T[:, n_speakers - 1] == 0.0, then S_arr[n_speakers - 1] == -1,
+        # # so S_arr[n_speakers - 1] is not used for training,
+        # # e.g., in the case of training 3-spk model with 2-spk data
+
+        # Y = feature.transform(Y, self.input_transform)
+        # Y_spliced = feature.splice(Y, self.context_size)
+        # Y_ss, T_ss = feature.subsample(Y_spliced, T, self.subsampling)
+        # ilen = np.array(Y_ss.shape[0], dtype=np.int64)
+
+        # return Y_ss, T_ss, S_arr, self.all_n_speakers_arr, ilen
+        rec_list = self.chunk_indices[i]
+        wav_data_list = []
+
+        for rec, st, ed in rec_list:
+            filtered_segments = self.data.segments[rec]
+            # speakers: the value given from data
+            speakers = np.unique(
+                [self.data.utt2spk[seg['utt']] for seg in filtered_segments]
+                ).tolist()
+            n_speakers = self.n_speakers
+            if self.n_speakers < len(speakers):
+                n_speakers = len(speakers)
+
+            Y, T = feature.get_labeledSTFT(
+                self.data,
+                rec,
+                st,
+                ed,
+                self.frame_size,
+                self.frame_shift,
+                n_speakers,
+                )
+            T = T.astype(np.float32)
+
+            S_arr = -1 * np.ones(n_speakers).astype(np.int64)
+            for seg in filtered_segments:
+                speaker_index = speakers.index(self.data.utt2spk[seg['utt']])
+                all_speaker_index = self.all_speakers.index(
+                    self.data.utt2spk[seg['utt']])
+                S_arr[speaker_index] = all_speaker_index
+
+            # If T[:, n_speakers - 1] == 0.0, then S_arr[n_speakers - 1] == -1,
+            # so S_arr[n_speakers - 1] is not used for training,
+            # e.g., in the case of training 3-spk model with 2-spk data
+
+            Y = feature.transform(Y, self.input_transform)
+            Y_spliced = feature.splice(Y, self.context_size)
+            Y_ss, T_ss = feature.subsample(Y_spliced, T, self.subsampling)
+            ilen = np.array(Y_ss.shape[0], dtype=np.int64)
+            wav_data = {}
+            wav_data['xs'] = Y_ss
+            wav_data['ts'] = T_ss
+            wav_data['ss'] = S_arr
+            wav_data['ns'] = self.all_n_speakers_arr
+            wav_data['ilens'] = ilen
+            wav_data['rec'] = rec
+            wav_data_list.append(wav_data)
+
+        return wav_data_list
+
+    def get_allnspk(self):
+        return self.all_n_speakers
 
 def collate_fn_ns(batch, n_speakers, spkidx_tbl):
     xs, ts, ss, ns, ilens = list(zip(*batch)) # feature, activity, speaker ID, speaker number, chunksize
@@ -197,29 +360,6 @@ def save_feature(args):
             )
     n_chunks = len(trainloader)
     print("n_chunks : {}".format(n_chunks))
-    idx = 0
-    for data in trainloader:
-        dir_num = idx // max_num_per_dir
-        os.makedirs("{}/data/{:0={}}/".
-                    format(args.model_save_dir, dir_num, digit_num),
-                    exist_ok=True)
-        output_npy_path = fmt.format(args.model_save_dir,
-                                     dir_num, digit_num, idx)
-        print(output_npy_path)
-        bs = data[0].shape[0] #batch size
-        cs = data[0].shape[1] #chunk size
-        # data0 (feature)
-        data0 = data[0]
-        # data1 (reference speech activity)
-        data1 = data[1]
-        # data2 (reference speaker ID)
-        data2 = np.zeros([bs, cs, data[2].shape[1]], dtype=np.float32)
-        for j in range(bs):
-            data2[j, :, :] = data[2][j, :]
-        # data3 (reference number of all speakers)
-        data3 = np.ones([bs, cs, 1], dtype=np.float32) * len(data[3][0])
-        # data4 (real chunk size)
-        data4 = np.zeros([bs, cs, 1], dtype=np.float32)
     os.makedirs("{}/data/".format(args.model_save_dir), exist_ok=True)
     f = open('{}/data/n_chunks.txt'.format(args.model_save_dir), 'w')
     f.write("{}\n".format(n_chunks))
@@ -238,10 +378,9 @@ def save_feature(args):
         npz = np.load(args.spkv_lab)
         spkidx_tbl = npz['arr_2']
 
-    torch.manual_seed(args.seed)
     trainloader = torch.utils.data.DataLoader(
             train_set, batch_size=batchsize,
-            shuffle=True, num_workers=args.num_workers,
+            shuffle=False, num_workers=args.num_workers,
             collate_fn=partial(
                 collate_fn_ns,
                 n_speakers=args.num_speakers,
@@ -294,11 +433,87 @@ def save_feature(args):
     f.close()
     print('Finished!')
 
-save_feature(args)
 
-def set_field_chunk_idx(ref):
-    field_idx_list = [0]
-    for i in range(len(ref)-1):
-        if ref[i + 1] != ref[i]:
-            field_idx_list.append(i + 1)
-    return field_idx_list
+def collate_fn_test(batch, n_speakers, spkidx_tbl):
+    data_list = list(*batch)
+    ms = len(batch)
+    bs = len(data_list)
+    cs = None
+    new_data = None
+    for data in data_list:
+        xs = data['xs']
+        ts = data['ts']
+        ss = data['ss']
+        ns = data['ns']
+        ilens = data['ilens']
+        
+
+        xs = np.array([np.pad(xs, [(0, np.max(ilens) - len(xs)), (0, 0)],
+                          'constant', constant_values=(-1,))])
+        ts = np.array([np.pad(ts, [(0, np.max(ilens) - len(ts)), (0, 0)],
+                          'constant', constant_values=(+1,))])
+
+
+        if cs == None:
+            cs = ts.shape[1]
+            data0 = xs
+            data1 = ts
+            data2 = np.zeros([1,cs, ts.shape[2]], dtype=np.float32)
+            for j in range(cs):
+                data2[0, j, :] = ss[:]
+            data3 = np.ones([1, cs, 1], dtype=np.float32) * len(ns)
+            data4 = np.zeros([1, cs, 1], dtype=np.float32)
+            for j in range(cs):
+                data4[0, j, :] = ilens
+
+            new_data = np.concatenate((data0,
+                                    data1,
+                                    data2,
+                                    data3,
+                                    data4), axis=2)
+        else:
+            cs = ts.shape[1]
+            data0 = xs
+            data1 = ts
+            data2 = np.zeros([1,cs, ts.shape[2]], dtype=np.float32)
+            for j in range(cs):
+                data2[0, j, :] = ss[:]
+            data3 = np.ones([1, cs, 1], dtype=np.float32) * len(ns)
+            data4 = np.zeros([1, cs, 1], dtype=np.float32)
+            for j in range(cs):
+                data4[0, j, :] = ilens
+
+            tmp_data = np.concatenate((data0,
+                                    data1,
+                                    data2,
+                                    data3,
+                                    data4), axis=2)       
+            new_data = np.concatenate((new_data, tmp_data), axis=0)                
+        
+    return new_data
+
+dataset = DiarizationDatasetFromWaveTest(
+    args.train_data_dir,
+    chunk_size=args.chunk_size,
+    context_size=args.context_size,
+    input_transform=args.input_transform,
+    frame_size=args.frame_size,
+    frame_shift=args.frame_shift,
+    subsampling=args.subsampling,
+    rate=args.sampling_rate,
+    n_speakers=args.num_speakers,
+)
+
+
+
+dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=1,
+        shuffle=True, num_workers=args.num_workers,
+        collate_fn=partial(
+                    collate_fn_test,
+                    n_speakers=args.num_speakers,
+                    spkidx_tbl=None)
+)
+
+for data in dataloader:
+    tmp = data
